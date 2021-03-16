252916c237bb3bcee05227ebb583b43e756dade2
Jennifer Long <j.alicia.long@gmail.com>
2021-03-16 00:36:06 -0400

    Fix routing

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'meals', component: MealsComponent }
+  { path: 'meals', component: MealsComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
 
